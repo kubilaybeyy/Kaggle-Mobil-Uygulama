7bc30f59c9c5f03eebc3f4b89a7c3fb3608d3d37
kubilaybeyy <a.kubilay2003@gmail.com>
2025-01-04 17:06:30 +0300

    last changes

--- KotlinApp/app/src/main/java/TensorFlowModel.kt
+++ KotlinApp/app/src/main/java/TensorFlowModel.kt
@@ -2,17 +2,19 @@ package com.example.FitnessApp
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Color
 import org.tensorflow.lite.Interpreter
-import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.nio.channels.FileChannel
 
 class TensorFlowModel(context: Context) {
     private var interpreter: Interpreter? = null
+    private val classNames = listOf(
+        "Elma", "Muz", "Karambola", "Guava", "Kivi", "Portakal", "Şeftali", "Armut", "Hurma", "Pitaya", "Erik", "Nar", "Domates", "Kavun" )
 
     init {
-        val modelPath = "model.tflite" // assets/model.tflite
+        val modelPath = "model.tflite" // Yeni model dosyasının adı
         val assetFileDescriptor = context.assets.openFd(modelPath)
         val fileInputStream = assetFileDescriptor.createInputStream()
         val fileChannel = fileInputStream.channel
@@ -22,39 +24,68 @@ class TensorFlowModel(context: Context) {
         interpreter = Interpreter(modelBuffer)
     }
 
-    fun predict(bitmap: Bitmap): String {
+    fun predict(input: Bitmap): String {
+        return try {
+            // Giriş tensörünü hazırla
+            val tensorInput = prepareTensorFromBitmap(input)
 
-        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, 200, 200, true)
+            // Çıkış tensörünü oluştur
+            val outputBuffer = TensorBuffer.createFixedSize(
+                intArrayOf(1, 26),
+                org.tensorflow.lite.DataType.FLOAT32
+            )
 
-        val normalizedBitmap = normalizeBitmap(resizedBitmap)
-        val tensorImage = TensorImage.fromBitmap(normalizedBitmap)
+            // Modeli çalıştır
+            interpreter?.run(tensorInput, outputBuffer.buffer)
 
-        val inputBuffer = tensorImage.tensorBuffer
-        val outputBuffer = TensorBuffer.createFixedSize(intArrayOf(1, 4), org.tensorflow.lite.DataType.FLOAT32)
+            // Çıkış tensöründeki verileri işle
+            val outputArray = outputBuffer.floatArray
+            val top3Indices = outputArray
+                .mapIndexed { index, confidence -> index to confidence }
+                .sortedByDescending { it.second }
+                .take(3)
 
-        interpreter?.run(inputBuffer.buffer, outputBuffer.buffer)
-
-        val outputArray = outputBuffer.floatArray
-        val maxIndex = outputArray.indices.maxByOrNull { outputArray[it] } ?: -1
-        return "Predicted Class: $maxIndex with Confidence: ${outputArray[maxIndex]}"
+            // En iyi 3 tahmini string olarak döndür
+            top3Indices.joinToString("\n") { (index, confidence) ->
+                "${classNames[index]}: %.2f%%".format(confidence * 100)
+            }
+        } catch (e: Exception) {
+            val inputTensorShape = interpreter?.getInputTensor(0)?.shape() // Giriş tensör şekli
+            val outputTensorShape = interpreter?.getOutputTensor(0)?.shape() // Çıkış tensör şekli
+            println("${inputTensorShape?.contentToString()}\n${outputTensorShape?.contentToString()}")
+            println(e.message)
+            "Prediction Error: ${e.message}"
+        }
     }
 
-    private fun normalizeBitmap(bitmap: Bitmap): Bitmap {
-        val normalizedBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
-        for (y in 0 until bitmap.height) {
-            for (x in 0 until bitmap.width) {
-                val pixel = bitmap.getPixel(x, y)
-                val r = Color.red(pixel) / 255.0f
-                val g = Color.green(pixel) / 255.0f
-                val b = Color.blue(pixel) / 255.0f
-                val normalizedColor = Color.rgb((r * 255).toInt(), (g * 255).toInt(), (b * 255).toInt())
-                normalizedBitmap.setPixel(x, y, normalizedColor)
+
+    private fun prepareTensorFromBitmap(bitmap: Bitmap): ByteBuffer {
+        val width = 224
+        val height = 224
+
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, true)
+
+        // TensorFlow Lite için ByteBuffer oluştur
+        val byteBuffer = ByteBuffer.allocateDirect(4 * width * height * 3) // 4 byte (float32), 3 kanal (RGB)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        // Bitmap'i ByteBuffer'a dönüştür
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val pixel = resizedBitmap.getPixel(x, y)
+                byteBuffer.putFloat((pixel shr 16 and 0xFF) / 255.0f) // R
+                byteBuffer.putFloat((pixel shr 8 and 0xFF) / 255.0f)  // G
+                byteBuffer.putFloat((pixel and 0xFF) / 255.0f)        // B
             }
         }
-        return normalizedBitmap
+
+        return byteBuffer
     }
+
+
 
     fun close() {
         interpreter?.close()
+
     }
 }
--- KotlinApp/app/src/main/java/com/example/FitnessApp/MainActivity.kt
+++ KotlinApp/app/src/main/java/com/example/FitnessApp/MainActivity.kt
@@ -1,69 +1,110 @@
 package com.example.FitnessApp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
-import com.example.FitnessApp.ui.theme.FitnessAppTheme
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var model: TensorFlowModel
+    private val CAMERA_PERMISSION_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        println("bitmap")
+
+        // TensorFlow modelini yükle
         model = TensorFlowModel(this)
 
-        val selectImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-            uri?.let {
-                val inputStream = contentResolver.openInputStream(it)
-                val bitmap = BitmapFactory.decodeStream(inputStream)
+        var selectedImage by mutableStateOf<Bitmap?>(null)
+        var predictionResult by mutableStateOf("No prediction yet")
 
-                val resizedBitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, true)
-                println(resizedBitmap)
-
-
-                val result = model.predict(resizedBitmap)
-                println(result)
-            }
-        }
-
-        val predictionResult = mutableStateOf("No prediction yet")
-
-        setContent {
-            AppContent(
-                predictionText = predictionResult.value,
-                onPickImageClick = {
-                    selectImageLauncher.launch("image/*")
-                }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_REQUEST_CODE
             )
         }
 
+        // Resim seçme işlemi
+        val selectImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                try {
+                    val inputStream = contentResolver.openInputStream(it)
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+
+                    // Tahmin yap
+                    val result = model.predict(bitmap)
+                    selectedImage = bitmap
+                    predictionResult = result
+                } catch (e: Exception) {
+                    Log.e("Prediction", "Tahmin işlemi sırasında hata oluştu: ${e.message}")
+                }
+            }
+        }
+
+        setContent {
+            AppContent(
+                predictionText = predictionResult,
+                selectedImage = selectedImage,
+                onPickImageClick = {
+                    selectImageLauncher.launch("image/*")
+                },
+                onTakePhotoClick = { bitmap ->
+                    // Tahmin yap
+                    selectedImage = bitmap
+                    predictionResult = model.predict(bitmap)
+                }
+            )
+        }
     }
 
 
-    private fun loadImageFromUri(uri: android.net.Uri): Bitmap {
-        val inputStream = contentResolver.openInputStream(uri)
-        return android.graphics.BitmapFactory.decodeStream(inputStream)
+    private fun prepareBitmapForTensor(bitmap: Bitmap): Bitmap {
+        val width = 224
+        val height = 224
+        return Bitmap.createScaledBitmap(bitmap, width, height, true)
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -72,7 +113,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AppContent(predictionText: String, onPickImageClick: () -> Unit) {
+fun CameraPreview(
+    onImageCaptured: (Bitmap) -> Unit,
+    onClose: () -> Unit
+) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var imageCapture: ImageCapture? = remember { null }
+
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(factory = { context ->
+            val previewView = PreviewView(context)
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (e: Exception) {
+                Log.e("CameraX", "Camera binding failed", e)
+            }
+
+            previewView
+        })
+
+        // Fotoğraf çekme düğmesi
+        Button(
+            onClick = {
+                val file = File(context.cacheDir, "temp_image.jpg")
+                val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+                imageCapture?.takePicture(
+                    outputOptions,
+                    ContextCompat.getMainExecutor(context),
+                    object : ImageCapture.OnImageSavedCallback {
+                        override fun onError(exception: ImageCaptureException) {
+                            Log.e("CameraX", "Photo capture failed", exception)
+                        }
+
+                        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                            onImageCaptured(bitmap)
+                        }
+                    }
+                )
+            },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Text("Fotoğraf Çek ve Kaydet")
+        }
+
+        // Kapatma düğmesi
+        Button(
+            onClick = { onClose() },
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+        ) {
+            Text("Kapat")
+        }
+    }
+}
+
+@Composable
+fun AppContent(
+    predictionText: String,
+    selectedImage: Bitmap?,
+    onPickImageClick: () -> Unit,
+    onTakePhotoClick: (Bitmap) -> Unit
+) {
+    // Kamera önizleme ekranını kontrol eden durum
+    var showCamera by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -80,35 +207,42 @@ fun AppContent(predictionText: String, onPickImageClick: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = predictionText, modifier = Modifier.padding(16.dp))
+        Text(
+            text = predictionText,
+            modifier = Modifier.padding(16.dp)
+        )
 
-        Button(onClick = onPickImageClick) {
-            Text(text = "Pick an Image")
+        selectedImage?.let {
+            Image(
+                bitmap = it.asImageBitmap(),
+                contentDescription = "Selected Image",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(200.dp)
+            )
         }
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun AppContentPreview() {
-    FitnessAppTheme {
-        AppContent(predictionText = "No prediction yet") {}
-    }
-}
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(onClick = onPickImageClick) {
+                Text(text = "Galeriden Fotoğraf Seç")
+            }
+            Button(onClick = { showCamera = true }) {  // Kamera önizlemesini aç
+                Text(text = "Fotoğraf Çek")
+            }
+        }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnessAppTheme {
-        Greeting("Android")
+        // Kamera önizleme ekranını göster
+        if (showCamera) {
+            CameraPreview(
+                onImageCaptured = { bitmap ->
+                    onTakePhotoClick(bitmap)
+                    showCamera = false  // Kamera ekranını kapat
+                },
+                onClose = { showCamera = false }  // Kullanıcı kapatmak isterse
+            )
+        }
     }
 }
